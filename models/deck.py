ad4d0e5d3a6ca3e5eba7594cf4c7444b0f80fd3a
shane_william_miller <shanemiller@iinet.net.au>
2024-12-13 19:43:03 +0800

    fixes to references to formats in deck.py

--- models/deck.py
+++ models/deck.py
@@ -1,4 +1,5 @@
 from init import db
+from models.format import Format
 
 class Deck(db.Model):
     __tablename__ = 'decks'
@@ -6,10 +7,10 @@ class Deck(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text)
-    format_id = db.Column(db.Integer, db.ForeignKey("formats.format_id"), nullable=False)
+    format_id = db.Column(db.Integer, db.ForeignKey('formats.id'))
 
     # Relationships
-    format = db.relationship('Format', back_populates='decks')
+    format = db.relationship('Format', backref='decks')
     deckbox_id = db.Column(db.Integer, db.ForeignKey("deckboxes.id"))
     deckbox = db.relationship("DeckBox", back_populates="decks")
     cards = db.relationship("DeckCard", back_populates="deck", lazy="dynamic")
