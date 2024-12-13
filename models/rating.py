96f074cb27c7a0304f43346830b6749db630c51d
shane_william_miller <shanemiller@iinet.net.au>
2024-12-13 20:27:21 +0800

    fix to ratings.py reference to deck not decks

--- models/rating.py
+++ models/rating.py
@@ -2,10 +2,10 @@ from init import db, ma
 
 # Rating Model
 class Rating(db.Model):
-    __tablename__ = 'rating'
+    __tablename__ = 'ratings'
 
     id = db.Column(db.Integer, primary_key=True)  # Unique rating ID
-    deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'), nullable=False)  # FK to Deck
+    deck_id = db.Column(db.Integer, db.ForeignKey('decks.id'), nullable=False)  # FK to Deck
     score = db.Column(db.Integer, nullable=False)  # Rating score (1–5)
     comment = db.Column(db.Text, nullable=True)  # Optional comment about the rating
     created_at = db.Column(db.DateTime, default=db.func.current_timestamp())  # Created time
