c7dc05c88b3f2ea97e82e3add622e07c35fd31c2
shane_william_miller <shanemiller@iinet.net.au>
2024-12-13 19:52:08 +0800

    fix to relationship between deck and deckbox specifying foriegn key for format id

--- models/deck.py
+++ models/deck.py
@@ -1,17 +1,17 @@
 from init import db
 from models.format import Format
+from models.deckbox import DeckBox
 
 class Deck(db.Model):
     __tablename__ = 'decks'
-
+    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200))
     format_id = db.Column(db.Integer, db.ForeignKey('formats.id'))
-
-    # Relationships
+    deckbox_id = db.Column(db.Integer, db.ForeignKey('deckboxes.id'))
+    
     format = db.relationship('Format', backref='decks')
-    deckbox_id = db.Column(db.Integer, db.ForeignKey("deckboxes.id"))
-    deckbox = db.relationship("DeckBox", back_populates="decks")
+    deckbox = db.relationship('DeckBox', backref='decks')
     cards = db.relationship("DeckCard", back_populates="deck", lazy="dynamic")
     ratings = db.relationship("Rating", back_populates="deck", lazy="dynamic")
