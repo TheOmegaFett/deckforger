0836b4a6a51b86cb71cf1ecec9297dfd24b1453a
shane_william_miller <shanemiller@iinet.net.au>
2024-12-15 13:45:16 +0800

    change feild to ma

--- schemas/card_schema.py
+++ schemas/card_schema.py
@@ -1,14 +1,13 @@
 '''Schema for serializing and deserializing Pokemon TCG cards'''
 
-from dataclasses import fields
 from init import ma
 from marshmallow import validates, ValidationError
 from schemas.cardtype_schema import CardTypeSchema
 from schemas.cardset_schema import SetSchema
 
 class CardSchema(ma.Schema):
-    cardtype = fields.Nested(CardTypeSchema)
-    sets = fields.Nested(CardSetSchema)
+    cardtype = ma.Nested(CardTypeSchema)
+    sets = ma.Nested(CardSetSchema)
     
     class Meta:
         fields = ('id', 'name', 'cardtype', 'set_id', 'sets')
