20a3714a9e95f7d76e838f032734bedc5e1f8f7e
shane_william_miller <shanemiller@iinet.net.au>
2024-12-16 06:04:10 +0800

    FIX to cardtype controller

--- controllers/cardtype_controller.py
+++ controllers/cardtype_controller.py
@@ -93,7 +93,7 @@ def get_type_popularity():
                 func.count(DeckCard.id).label('usage_count')
             )
             .select_from(CardType)
-            .join(Card, Card.type_id == CardType.id)
+            .join(Card, Card.cardtype_id == CardType.id)
             .join(DeckCard, DeckCard.card_id == Card.id)
             .group_by(CardType.id, CardType.name)
             .order_by(desc('usage_count'))
@@ -103,7 +103,7 @@ def get_type_popularity():
         return jsonify([{
             'card_type': cardtype[0].name,
             'usage_count': cardtype[1]
-        } for cardtype in results]), 200    
+        } for cardtype in results]), 200
     except Exception as e:
         return jsonify({'error': 'Failed to calculate type popularity', 'details': str(e)}), 500
 
