035432002287dbfd80e58944bf5d55eae1b47867
shane_william_miller <shanemiller@iinet.net.au>
2024-12-15 14:27:50 +0800

    changed queries in cardtype controller

--- controllers/cardtype_controller.py
+++ controllers/cardtype_controller.py
@@ -8,19 +8,24 @@ cardtype_controller = Blueprint('cardtypes', __name__, url_prefix='/cardtypes')
 @cardtype_controller.route('/', methods=['GET'])
 def get_all_types():
     """Get all card types"""
-    types = CardType.query.all()
+    stmt = db.select(CardType)
+    types = db.session.scalars(stmt).all()
     return cardtypes_schema.dump(types)
 
 @cardtype_controller.route('/search/<string:name>', methods=['GET'])
 def search_by_name(name):
     """Search for types by name"""
-    types = CardType.query.filter(CardType.name.ilike(f'%{name}%')).all()
+    stmt = db.select(CardType).filter(CardType.name.ilike(f'%{name}%'))
+    types = db.session.scalars(stmt).all()
     return cardtypes_schema.dump(types)
 
 @cardtype_controller.route('/<int:type_id>/cards', methods=['GET'])
 def get_cards_by_type(type_id):
     """Get all cards of a specific type"""
-    card_type = CardType.query.get_or_404(type_id)
+    stmt = db.select(CardType).filter_by(id=type_id)
+    card_type = db.session.scalar(stmt)
+    if not card_type:
+        return jsonify({'error': 'Card type not found'}), 404
     return jsonify([{
         'id': card.id,
         'name': card.name,
