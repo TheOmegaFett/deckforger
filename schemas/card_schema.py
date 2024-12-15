1ac12eb40d3afeeb810dbb61d3c3c5c25dea5f4d
shane_william_miller <shanemiller@iinet.net.au>
2024-12-15 15:02:49 +0800

    changed to cardset"

--- schemas/card_schema.py
+++ schemas/card_schema.py
@@ -7,15 +7,13 @@ from schemas.cardset_schema import SetSchema
 
 class CardSchema(ma.Schema):
     cardtype = ma.Nested(CardTypeSchema)
-    sets = ma.Nested(SetSchema)
+    cardset = ma.Nested(SetSchema)
     
     class Meta:
-        fields = ('id', 'name', 'cardtype', 'set_id')
+        fields = ('id', 'name', 'cardtype', 'cardset')
         ordered = True
     
-    # Relationships
-    cardset = ma.relationship("CardSet", backref="cards")
-
+    
 # Schema instances
 card_schema = CardSchema()
 cards_schema = CardSchema(many=True)
