7f1dd56025927d8f44033d9eaa32c12ee0887d62
shane_william_miller <shanemiller@iinet.net.au>
2024-12-21 04:58:46 +0800

    change to cardset_controller

--- controllers/cardset_controller.py
+++ controllers/cardset_controller.py
@@ -37,7 +37,7 @@ def get_all_sets():
         )
         
         return jsonify({
-            "sets": cardset_schema.dump(pagination.items),  # Using cardset_schema instead of cardsets_schema
+            "sets": cardsets_schema.dump(pagination.items),
             "pagination": {
                 "total": pagination.total,
                 "pages": pagination.pages,
